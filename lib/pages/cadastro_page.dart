41069ef591327b9057460c3b7012346f4d74274e
LamenLuan <luan.leme.s35@gmail.com>
2021-07-14 18:17:38 -0300

    Criando tela de cadastro

---
+++ lib/pages/cadastro_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CadastroPage extends StatefulWidget {
+  CadastroPage({Key? key}) : super(key: key);
+
+  @override
+  _CadastroPageState createState() => _CadastroPageState();
+}
+
+class _CadastroPageState extends State<CadastroPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: null,
+    );
+  }
+}
