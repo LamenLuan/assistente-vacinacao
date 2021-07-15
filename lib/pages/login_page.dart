5eb27f3e81f4bf97b46c3013d96598d59e5b5b32
LamenLuan <luan.leme.s35@gmail.com>
2021-07-15 17:02:54 -0300

    Adicionando um componente de página para formulário

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:assistente_vacinacao/components/botao.dart';
 import 'package:assistente_vacinacao/components/campo_entrada.dart';
-import 'package:assistente_vacinacao/pages/cadastro_page.dart';
+import 'package:assistente_vacinacao/components/pagina_formulario.dart';
+import 'package:assistente_vacinacao/pages/cadastro_pessoa/cadastro_pessoa1_page.dart';
 import 'package:assistente_vacinacao/pages/principal_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -13,56 +14,44 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final _form = GlobalKey<FormState>();
-
+  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Assistente de Vacinação'),
-        centerTitle: true
-      ),
-      body: Form(
-        key: _form,
-        child: Padding(
-          padding: const EdgeInsets.all(48),
-          child: Column(
-            children: [
-              CampoEntrada(
-                titulo: 'CPF',
-                enableSuggestions: false,
-                autocorrect: false,
-                keyboardType: TextInputType.number,
-                inputFormatters: [
-                  FilteringTextInputFormatter.digitsOnly,
-                ],
-              ),
-              CampoEntrada(
-                titulo: 'Senha',
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-              ),
-              Botao(
-                titulo: 'Entrar',
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(
-                    builder: (_) => PrincipalPage()
-                  ));
-                }
-              ),
-              Botao(
-                titulo: 'Cadastrar',
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(
-                    builder: (_) => CadastroPage()
-                  ));
-                }
-              )
-            ],
-          ),
+    return PaginaFormulario(
+      titulo: 'Assitente de Vacinação',
+      children: [
+        CampoEntrada(
+          titulo: 'CPF',
+          enableSuggestions: false,
+          autocorrect: false,
+          keyboardType: TextInputType.number,
+          inputFormatters: [
+            FilteringTextInputFormatter.digitsOnly,
+          ],
+        ),
+        CampoEntrada(
+          titulo: 'Senha',
+          obscureText: true,
+          enableSuggestions: false,
+          autocorrect: false,
+        ),
+        Botao(
+          titulo: 'Entrar',
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(
+              builder: (_) => PrincipalPage()
+            ));
+          }
+        ),
+        Botao(
+          titulo: 'Cadastrar',
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(
+              builder: (_) => CadastroPessoa1Page()
+            ));
+          }
         )
-      )
+      ]
     );
   }
 }
